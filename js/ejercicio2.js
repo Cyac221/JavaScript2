e6e8d5716bcf701363b3ccfbd36cd425acab4f3d
Carlos Yepes <carlosyepes021@gmail.com>
2025-07-26 14:24:31 -0500

    Otro commit mas para la prueba

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -35,15 +35,15 @@ console.log(`Neto --->: ${salario} `)
 
 //User ID
 
-const user = "Carlosyepes021"
-const password = "123456789"
+const user = "Paco"
+const password = "1234"
 
 console.log("Bienvenido al sistema de calificaciones")
-user_in = prompt("Ingrese su usuario: ")
-pass_in = prompt("Ingrese su Contraseña: ")
+let user_in = prompt("Ingrese su usuario: ")
+let pass_in = prompt("Ingrese su Contraseña: ")
 
 if (user_in == user && pass_in == password){
-    console.log("Contraseña correcta. Bienvenido")
+    console.log(`Contraseña correcta. Bienvenido ${user}` )
 }
 else{
     console.log("Contraseña y/o usuario incorrecto. Impostor!")
