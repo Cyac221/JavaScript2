04ab654c9d4de5a7550dccd6c9ff23fa369ffdb7
Carlos Yepes <carlosyepes021@gmail.com>
2025-07-26 11:19:42 -0500

    Primeros arranques JS

---
+++ js/notas.js
@@ -0,0 +1,10 @@
+//Datos de entrada
+
+let nombre_estudiante = prompt("Estudiante")
+let nota_conocimiento = parseFloat(prompt("Conocimiento"))
+let nota_desempeno = parseFloat(prompt("Desempeño"))
+let nota_prod = parseFloat(prompt("Producto"))
+
+const porcentaje_conocimiento = 0.2
+const porcentaje_desempeno = 0.3
+const porcentaje_producto = 0.5
