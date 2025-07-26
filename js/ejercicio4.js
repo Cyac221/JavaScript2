905ab1f8c554e66b2a05289a12266f9230292f51
Carlos Yepes <carlosyepes021@gmail.com>
2025-07-26 14:51:43 -0500

    Otra vuelta

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -5,18 +5,17 @@ if(numero_dia>=1 && numero_dia<=7){
         console.log("Lunes")
     }else if(numero_dia==2){
         console.log("Martes")
-    }elseif (numero_dia==3){
+    }else if (numero_dia==3){
         console.log("Miercoles")
-    }elseif (numero_dia==4){
+    }else if (numero_dia==4){
         console.log("Jueves")
-    }elseif (numero_dia==5){
+    }else if (numero_dia==5){
         console.log("Viernes")
-    }elseif(numero_dia==6){
+    }else if(numero_dia==6){
         console.log("Sabado")
-    }elseif(numero_dia==7){
-        console.log("Domingo")
-    }
-}
+    }else
+        console.log("Domingo")}
+
 else{
     console.log("Debe ser entre 1 y 7. Aguevado")
 }
