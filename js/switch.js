a8fa41cdcb59539736167ca2f8b68bb666e529df
Carlos Yepes <carlosyepes021@gmail.com>
2025-07-30 21:40:07 -0500

    Nuevo archivo

---
+++ js/switch.js
@@ -0,0 +1,24 @@
+// Switch
+
+let expresion =prompt("Ingreseuna vaina")
+
+
+switch (expresion) {
+    case 'valor1':
+        console.log("Este es el caso 1")
+        break;
+
+    case 'valor2':
+        console.log('Este es el caso 2')
+        break;
+
+    default:
+        console.log('Ingrese una opcion valida')
+}
+
+console.log('Fin del programa')
+console.log(expresion)
+
+
+
+
