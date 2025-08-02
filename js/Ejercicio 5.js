80267eaedffbad234c65a5005ad96f09f0fdfeca
Carlos Yepes <carlosyepes021@gmail.com>
2025-08-02 10:46:31 -0500

    Caso switch agregado nuevo en 5

---
+++ js/Ejercicio 5.js
@@ -0,0 +1,22 @@
+let nro = parseInt(prompt("Dia"))
+
+switch (nro) {
+    case 1:
+        console.log("Este es el caso 1")
+        break;
+
+    case 2:
+        console.log("Caso 2")
+        break;
+
+    case 3:
+        console.log("Caso 3")
+        break;
+    case 4:
+        console.log("Caso 4")
+        break;
+    default:
+        console.log("Concentraiiito' 😂")
+        break;
+}
+
